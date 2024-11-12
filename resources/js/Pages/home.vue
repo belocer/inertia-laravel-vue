2bae2f03abcd4874a44f4bd7487b2242053e12f5
Denis Belotserkovets <belocerkovecden@gmail.com>
2024-11-12 13:21:59 +0300

    first vue3

--- resources/js/Pages/home.vue
+++ resources/js/Pages/home.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { Head } from '@inertiajs/vue3';
 defineProps(['title'])
 </script>
 
 <template>
+    <Head :title="title" />
     <div>
         <h1>{{ title }}</h1>
     </div>
